9d9025c5a207f7442f8335bd7dcac3021fa3451d
umairconnect <moonsoftwizz@gmail.com>
2022-04-23 07:09:26 +0500

    create appointment tab added in admin role

--- resources/views/layouts/hor-menu.blade.php
+++ resources/views/layouts/hor-menu.blade.php
@@ -47,11 +47,24 @@
                                     class="dropdown-item">{{ __('Add New Receptionist') }}</a>
                             </div>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('pending-appointment') }}">
-                                <i class='bx bx-list-plus mr-2'></i>{{ __('Appointment List') }}
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-layout" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class='bx bx-list-plus mr-2'></i>{{ __('Appointment') }} <div
+                                        class="arrow-down"></div>
                             </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="topnav-layout">
+                                <a class="dropdown-item" href="{{ url('pending-appointment') }}">
+                                    {{ __('List of Appointment') }}
+                                </a>
+                                <a class="dropdown-item" href="{{ route('appointment.create') }}">
+                                    {{ __('Create Appointment') }}
+                                </a>
+                            </div>
                         </li>
+
+
                         <li class="nav-item">
                             <a class="nav-link" href="{{ url('transaction') }}">
                                 <i class='bx bx-list-check mr-2'></i>{{ __('Transaction') }}
--- storage/framework/views/2691f12a172b8ab25ad067665d31db307c7b629a.php
+++ storage/framework/views/2691f12a172b8ab25ad067665d31db307c7b629a.php
@@ -48,12 +48,26 @@
                                     class="dropdown-item"><?php echo e(__('Add New Receptionist')); ?></a>
                             </div>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo e(url('pending-appointment')); ?>">
-                                <i class='bx bx-list-plus mr-2'></i><?php echo e(__('Appointment List')); ?>
 
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-layout" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class='bx bx-list-plus mr-2'></i><?php echo e(__('Appointment')); ?> <div
+                                        class="arrow-down"></div>
                             </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="topnav-layout">
+                                <a class="dropdown-item" href="<?php echo e(url('pending-appointment')); ?>">
+                                    <?php echo e(__('List of Appointment')); ?>
+
+                                </a>
+                                <a class="dropdown-item" href="<?php echo e(route('appointment.create')); ?>">
+                                    <?php echo e(__('Create Appointment')); ?>
+
+                                </a>
+                            </div>
                         </li>
+
+
                         <li class="nav-item">
                             <a class="nav-link" href="<?php echo e(url('transaction')); ?>">
                                 <i class='bx bx-list-check mr-2'></i><?php echo e(__('Transaction')); ?>
